733982d62e2af36f52f6ec71714cdd48a55a5011
egublog <egukei0729@gmail.com>
2022-04-05 04:04:39 +0900

    [ADD] storeの実装

--- tasklist/src/main/java/jp/gihyo/profava/tasklist/HomeRestController.java
+++ tasklist/src/main/java/jp/gihyo/profava/tasklist/HomeRestController.java
@@ -1,10 +1,13 @@
 package jp.gihyo.profava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
@@ -18,5 +21,14 @@ public class HomeRestController {
         It works!
         現在時刻は%sです。
         """.formatted(LocalDateTime.now());
+  }
+
+  @GetMapping("/add")
+  String addItem(@RequestParam("task") String task,
+                 @RequestParam("deadline") String deadline) {
+    String id = UUID.randomUUID().toString().substring(0, 8);
+    TaskItem item = new TaskItem(id, task, deadline, false);
+    taskItems.add(item);
+    return "タスクを追加しました。";
   }
 }
